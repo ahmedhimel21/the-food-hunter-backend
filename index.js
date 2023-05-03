b36e10fb77c22ee98774299bb0c41a2339181f4a
ahmedhimel21 <ahmedhimel000@gmail.com>
2023-05-03 18:28:02 +0600

    chef json file created

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+const chef = require('./data/chef.json');
+
 app.use(cors());
 
 app.get(
@@ -12,6 +14,15 @@ app.get(
   }
 );
 
+app.get('/chef',(req,res) =>{
+  res.send(chef)
+})
+
+app.get('/chef/:id',(req,res) =>{
+  const id = req.params.id;
+  const singleChef = chef.find(c => c.id ==id);
+  res.send(singleChef)
+})
 app.listen(
   port, () =>{
     console.log(`server is running on port: ${port}`)
